737c4751edf7c9a1d4f341ddc700058c02796cd0
prashantkumarll <prashantkumarll@gmail.com>
2018-11-19 22:09:57 +0530

    message

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req,res) => {
     })
 });
 
+app.get('/todos', (req,res) => {
+
+    Todo.find({}).then((data)=>{ res.send(data);},(err)=>{res.status(400).send(err);});
+
+});
+
+
+
 app.listen(port, () =>{
     console.log('Started on port 3000');
 })
